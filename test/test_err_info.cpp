38c9af279f1004885e6f57cd1eafc9aaef9f34e3
Robert King <varitonium@gmail.com>
2026-07-09 19:37:44 +0200

    TCX-4: dev-err utils macro impl

--- test/test_err_info.cpp
+++ test/test_err_info.cpp
@@ -3,10 +3,19 @@
 
 #include <doctest/doctest.h>
 
+#include <err_utils.h>
 #include <errty.h>
 #include <intervalerr.h>
 
 TEST_CASE("IntervalScannerError topology enum class") {
   static_assert(topology::ErrorInfoTy<math::IntervalErrorInfo>);
+  CHECK(true);
+}
+
+TEST_CASE("DefineErrorInfo macro") {
+  enum class ObliviousError {};
+  ERR_DEFINE_ERROR_INFO(ObliviousError, ObliviousErrorInfo);
+  static_assert(
+      std::same_as<ObliviousErrorInfo, err::ErrorInfo<ObliviousError>>);
   CHECK(true);
 }
