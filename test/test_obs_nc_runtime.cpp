19a954d4f8268d14dba4f1ca60d323d7a088eac0
Robert King <varitonium@gmail.com>
2026-07-22 23:38:32 +0200

    TCX-9: dev-nano macro impl

---
+++ test/test_obs_nc_runtime.cpp
@@ -0,0 +1,24 @@
+
+#define DOCTEST_CONFIG_IMPLEMENT
+
+#include <doctest/doctest.h>
+
+#include <runtime.h>
+#include <print>
+
+TEST_CASE("clock behavior")
+{
+    CHECK(1 + 1 == 2);
+}
+
+NANO_CLOCK_RUNTIME_MAIN
+{
+    clock();
+    clock();
+    //todo change the interface used to extract the elapsed time
+    const auto time = clock.elapsed();
+    std::println("{}",time);
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    return context.run();
+}
