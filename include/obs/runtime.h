70d0ef9e5ccddd8811f1aabd6b136a258810e75f
Robert King <varitonium@gmail.com>
2026-07-22 23:38:32 +0200

    TCX-9: dev-nano macro impl

--- include/obs/runtime.h
+++ include/obs/runtime.h
@@ -58,6 +58,22 @@ namespace obs{
     static int clock_runtime_main(                                      \
         int argc, char** argv, [[maybe_unused]] obs::micro_c& clock)
 
+#define NANO_CLOCK_RUNTIME_MAIN                                        \
+    static int clock_runtime_main(                                      \
+        int argc, char** argv, obs::micro_c& clock);                    \
+                                                                        \
+    int main(int argc, char** argv) {                                   \
+        if (argc == 1) {                                                \
+            std::println("missing argument");                           \
+            return EXIT_FAILURE;                                        \
+        }                                                               \
+                                                                        \
+        obs::micro_c clock;                                             \
+        return clock_runtime_main(argc, argv, clock);                   \
+    }                                                                   \
+                                                                        \
+    static int clock_runtime_main(                                      \
+        int argc, char** argv, [[maybe_unused]] obs::micro_c& clock)
 
 } //namespace obs
 
