7d289cf25abda9a1486a8b789fdfd4179bd14a70
Robert King <varitonium@gmail.com>
2026-08-09 23:14:22 +0200

    TCX-17: upe-amending of integration test

--- test/test_obs_nc_runtime.cpp
+++ test/test_obs_nc_runtime.cpp
@@ -11,9 +11,8 @@ TEST_CASE("clock behavior") { CHECK(1 + 1 == 2); }
 NANO_CLOCK_RUNTIME_MAIN {
   clock();
   clock();
-  /*todo change the interface used to extract the elapsed time*/
-  const auto time = clock.elapsed_to_str();
-  std::println("{}", time);
+  const auto time = clock.elapsed();
+  std::println("{} ns", time);
   doctest::Context context;
   context.applyCommandLine(argc, argv);
   return context.run();
