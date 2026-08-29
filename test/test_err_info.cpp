1e8e9cde391462da8212d69d79609e00dc390990
Robert King <varitonium@gmail.com>
2026-08-29 23:15:28 +0200

    TCX-22: dev-adding of templated err propagate function

--- test/test_err_info.cpp
+++ test/test_err_info.cpp
@@ -6,6 +6,81 @@
 #include <err_utils.h>
 #include <errty.h>
 #include <intervalerr.h>
+#include <ncnm.h>
+
+/*
+ * Define a class that possess an error info
+ * and a way for other class to find this error type
+ * at compile time
+ */
+
+enum class DecoyError { ValueError };
+
+ERR_DEFINE_ERROR_INFO(DecoyError, DecoyErrorInfo);
+
+class Decoy : public core::NCNM<Decoy> {
+public:
+  explicit Decoy(int id) noexcept : id_{id} {}
+  ~Decoy() = default;
+
+  std::expected<void, DecoyErrorInfo> compare_id(int val) const noexcept {
+    if (val == id_) {
+      return err::unexpected(DecoyError::ValueError, "value is not valid");
+    }
+    return {};
+  };
+
+private:
+  int id_;
+};
+
+enum class ClientError { ValueError };
+
+ERR_DEFINE_ERROR_INFO(ClientError, ClientErrorInfo);
+
+template <typename T, typename ERR>
+class Client : public core::NCNM<Client<T, ERR>> {
+
+public:
+  explicit Client(int id) noexcept : id_{id} {}
+  ~Client() = default;
+
+  std::expected<void, ERR> compare_id(int val) const noexcept {
+    T t{id_};
+    auto res = t.compare_id(val);
+    if (!res.has_value()) {
+      return err::propagate(res);
+    }
+    return {};
+  }
+
+private:
+  int id_;
+};
+
+enum class XClientError { ValueError };
+
+ERR_DEFINE_ERROR_INFO(XClientError, XClientErrorInfo);
+
+template <typename T, typename ERR>
+class XClient : public core::NCNM<XClient<T, ERR>> {
+
+public:
+  explicit XClient(int id) noexcept : id_{id} {}
+  ~XClient() = default;
+
+  std::expected<void, ERR> compare_id(int val) const noexcept {
+    T t{id_};
+    auto res = t.compare_id(val);
+    if (!res.has_value()) {
+      return err::propagate(res, XClientError::ValueError);
+    }
+    return {};
+  }
+
+private:
+  int id_;
+};
 
 TEST_CASE("IntervalScannerError topology enum class") {
   static_assert(topology::ErrorInfoTy<math::IntervalErrorInfo>);
@@ -18,4 +93,12 @@ TEST_CASE("DefineErrorInfo macro") {
   static_assert(
       std::same_as<ObliviousErrorInfo, err::ErrorInfo<ObliviousError>>);
   CHECK(true);
+}
+
+TEST_CASE("Propagate error with target") {
+  XClient<Decoy, XClientErrorInfo> d{1};
+  auto res = d.compare_id(1);
+  CHECK(!res.has_value());
+  CHECK(res.error().type == XClientError::ValueError);
+  CHECK(res.error().message == "value is not valid");
 }
