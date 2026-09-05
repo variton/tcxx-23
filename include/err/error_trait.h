95e800946fed3ce5d2e04be31f370eed2a9ff56c
Robert King <varitonium@gmail.com>
2026-09-05 22:37:09 +0200

    TCX-27: upe-error info documentation

--- include/err/error_trait.h
+++ include/err/error_trait.h
@@ -7,16 +7,14 @@
 namespace err {
 
 /** Default non-copyable, non-movable base type. */
-template <typename T>
-using Default = core::NCNM<T>;
+template <typename T> using Default = core::NCNM<T>;
 
 /**
  * @brief Provides error-type information.
  * @tparam T Type containing an `ErrType_` member type.
  */
-template <typename T>
-struct ErrorTrait : public Default<ErrorTrait<T>> {
-  using ErrTypeInfo = T;             ///< Error type-information provider.
+template <typename T> struct ErrorTrait : public Default<ErrorTrait<T>> {
+  using ErrTypeInfo = T;                ///< Error type-information provider.
   using ErrType = typename T::ErrType_; ///< Underlying error type.
 };
 
