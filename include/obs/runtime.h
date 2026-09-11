cd14f8a3fb00fd3341c40062326c4e50d666027e
Robert King <varitonium@gmail.com>
2026-09-11 23:34:49 +0200

    TCX-29: doc-runtime macro

--- include/obs/runtime.h
+++ include/obs/runtime.h
@@ -1,4 +1,3 @@
-
 #ifndef RUNTIME_H
 #define RUNTIME_H
 
@@ -7,6 +6,11 @@
 
 namespace obs {
 
+/**
+ * @brief Defines a program entry point using an `obs::s_clock`.
+ *
+ * The resulting function body receives `argc`, `argv`, and `clock`.
+ */
 #define CLOCK_RUNTIME_MAIN                                                     \
   static int clock_runtime_main(int argc, char **argv, obs::s_clock &clock);   \
                                                                                \
@@ -23,6 +27,7 @@ namespace obs {
   static int clock_runtime_main(int argc, char **argv,                         \
                                 [[maybe_unused]] obs::s_clock &clock)
 
+/** @brief Defines a program entry point using an `obs::milli_c` clock. */
 #define MILLI_CLOCK_RUNTIME_MAIN                                               \
   static int clock_runtime_main(int argc, char **argv, obs::milli_c &clock);   \
                                                                                \
@@ -39,6 +44,7 @@ namespace obs {
   static int clock_runtime_main(int argc, char **argv,                         \
                                 [[maybe_unused]] obs::milli_c &clock)
 
+/** @brief Defines a program entry point using an `obs::micro_c` clock. */
 #define MICRO_CLOCK_RUNTIME_MAIN                                               \
   static int clock_runtime_main(int argc, char **argv, obs::micro_c &clock);   \
                                                                                \
@@ -55,8 +61,9 @@ namespace obs {
   static int clock_runtime_main(int argc, char **argv,                         \
                                 [[maybe_unused]] obs::micro_c &clock)
 
+/** @brief Defines a program entry point using an `obs::nano_c` clock. */
 #define NANO_CLOCK_RUNTIME_MAIN                                                \
-  static int clock_runtime_main(int argc, char **argv, obs::nano_c &clock);   \
+  static int clock_runtime_main(int argc, char **argv, obs::nano_c &clock);    \
                                                                                \
   int main(int argc, char **argv) {                                            \
     if (argc == 1) {                                                           \
@@ -64,7 +71,7 @@ namespace obs {
       return EXIT_FAILURE;                                                     \
     }                                                                          \
                                                                                \
-    obs::nano_c clock;                                                        \
+    obs::nano_c clock;                                                         \
     return clock_runtime_main(argc, argv, clock);                              \
   }                                                                            \
                                                                                \
